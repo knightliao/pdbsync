bbb4b495cb939ef6b4c0984d76faa2cc6fcb1626
knightliao <knightliao@gmail.com>
2016-07-05 23:45:31 +0800

    re

--- pdbsync/cli/logo.py
+++ pdbsync/cli/logo.py
@@ -5,6 +5,8 @@ import os
 import pdbsync
 
 
+# http://www.kammerl.de/ascii/AsciiSignature.php
+
 def print_logo():
     logo_path = os.path.join(os.path.dirname(pdbsync.__file__), "logo.txt")
     try:
